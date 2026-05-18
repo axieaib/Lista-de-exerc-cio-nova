4a4bcaaf52aa04e1eac6c84751ad9fd9ba79c0c3
axieaib <axieterra12@gmail.com>
2026-05-18 20:35:10 -0300

    Update rectangle dimensions in index.php

--- 3/index.php
+++ 3/index.php
@@ -3,9 +3,9 @@
     require_once "Retangulo.php";
 
     $retangulo1 = new Retangulo();
-    $retangulo1->largura = 8.5;
-    $retangulo1->altura = 6;
+    $retangulo1->largura = 7.5;
+    $retangulo1->altura = 4;
 
-    echo "<p>Retângulo: {$retangulo1->largura} cm x {$retangulo1->altura} cm | Área: " . $retangulo1->calcularArea() . " | Perímetro: " . $retangulo1->calcularPerimetro() . " </p>";
+echo "<p>Retângulo: {$retangulo1->largura} cm x {$retangulo1->altura} cm | Área: " . $retangulo1->calcularArea() . " | Perímetro: " . $retangulo1->calcularPerimetro() . " </p>";
             
 ?>
