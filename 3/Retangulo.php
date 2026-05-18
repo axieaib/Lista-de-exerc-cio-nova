4933917d1e7b96eb80078f58d34ca68fd1e2f9a9
axieaib <axieterra12@gmail.com>
2026-05-18 20:34:34 -0300

    Update Retangulo.php

--- 3/Retangulo.php
+++ 3/Retangulo.php
@@ -4,14 +4,13 @@ class Retangulo{
     public float $largura;
     public float $altura;
 
-    public function calcularArea(){
+    
+public function calcularArea(){
         return ($this->altura * $this->largura);
     }
-
-    public function calcularPerimetro(){
+public function calcularPerimetro(){
         return ($this->largura + $this->altura) * 2;
     }
-
 }
 
 ?>
